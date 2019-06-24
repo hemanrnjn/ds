7f69023f30d149b31e0585112c919bbe0cedd257
hemanrnjn <himanshurnjn04@gmail.com>
2019-06-25 02:43:31 +0530

    Added Stack

---
+++ stack/stack.py
@@ -0,0 +1,31 @@
+from sys import maxsize
+
+def createStack():
+    stack = []
+    return stack
+
+def isEmpty(stack):
+    if len(stack) > 0:
+        return False
+    return True
+
+def push(stack, item):
+    stack.append(item)
+    return stack
+
+def pop(stack):
+    if isEmpty(stack):
+        return str(-maxsize-1)
+    return stack.pop()
+
+def peek(stack):
+    if isEmpty(stack):
+        return str(-maxsize-1)
+    return stack[len(stack) - 1]
+
+stack = createStack() 
+push(stack, str(10)) 
+push(stack, str(20)) 
+push(stack, str(30)) 
+print(pop(stack) + " popped from stack")
+print(stack)
